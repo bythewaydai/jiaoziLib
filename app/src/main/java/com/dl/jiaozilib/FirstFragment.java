79a8d9b0070c9ca18f3207931b87de51e29c6dcf
dl <manyouzhe@126.com>
2020-07-28 17:22:26 +0800

    lib添加

--- app/src/main/java/com/dl/jiaozilib/FirstFragment.java
+++ app/src/main/java/com/dl/jiaozilib/FirstFragment.java
@@ -22,7 +22,6 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
